ea73957cb9b90f5eb388d68f67a89c6a265d4e2d
RS882 <78429333+RS882@users.noreply.github.com>
2021-04-06 15:56:59 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,4 +4,10 @@ $(document).ready(function () {
 		$('.header__burger, .header__row, .header__logo, .header__contact-box').toggleClass('active'); // добавляется класс - при повторном- пропадает
 		$('body').toggleClass('lock');
 	});
+	$('.menu__link, .header__contact').click(function (event) {// при нажатии на .header__burger
+		$('.header__burger, .header__row, .header__logo, .header__contact-box').removeClass('active'); // добавляется класс - при повторном- пропадает
+		$('body').removeClass('lock');
+	});
+
+
 });
